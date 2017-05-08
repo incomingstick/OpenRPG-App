1c09e67cffd2b1d2bcfae9add57be2a7f6cf22ec
incomingstick <incomingstick@gmail.com>
2017-05-08 16:38:49 -0500

    compiling with jquery and bootstrap

--- app/main.js
+++ app/main.js
@@ -6,9 +6,9 @@ const url = require('url')
 // be closed automatically when the JavaScript object is garbage collected.
 let win
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({width: 1280, height: 720})
+  win = new BrowserWindow({ width: 1280, height: 720 })
 
   // and load the index.html of the app.
   win.loadURL(url.format({
