a07bc9df0fb8411764905d7676e74ea881d8246d
incomingstick <incomingstick@gmail.com>
2017-05-08 14:00:21 -0500

    started layout work

--- app/main.js
+++ app/main.js
@@ -8,7 +8,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 1280, height: 720})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -18,7 +18,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  //win.webContents.openDevTools()
+  win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
