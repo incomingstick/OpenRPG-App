343eb7b0119540a0f6a7caa05d84f924f6df753a
incomingstick <incomingstick@gmail.com>
2019-09-09 01:15:42 -0500

    npm audit fix

--- libWrappers/build.sh
+++ libWrappers/build.sh
@@ -1,10 +1,12 @@
 #TODO: Cleanup app/libWrapper build tree
-#TODO check to see if OpenRPG is already installed on the system
+#TODO: check to see if OpenRPG is already installed on the system
 # otherwise we should use the submodule. We will force use the SM
 # for now.
+#FIXME(incomingstick): currently the binding.gyp will only locate the
+#ORPG libs on Linux
 git submodule init
 git submodule update
 
 ../OpenRPG/build.sh
 
-node-gyp configure build --verbose
+../node_modules/.bin/node-gyp configure build --verbose
